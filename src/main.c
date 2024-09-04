9ad1973793b0cc07edc1ec94a84fa1b34f19f37c
xclusivesmash <sbumatshiane916@gmail.com>
2024-09-04 22:50:45 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <../dependencies/utils.h>
+
+/**
+  * main - general workflow of the process.
+  * @argc: argument count.
+  * @argv: argument vector.
+  * Return: 0 on success. Otherwise, -1.
+  */
+int main(int argc, char **argv)
+{
+	/* declarations */
+	(void) argc;
+	(void) argv;
+	/* workflow */
+	/* to be continued... */
+	return (0);
+}
