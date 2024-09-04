62cb2b5b75f00a9720656fb4174e897fdf3a2e84
xclusivesmash <sbumatshiane916@gmail.com>
2024-09-04 22:50:02 +0200

    Added header file

---
+++ dependencies/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/********** LIBRARIES **********/
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+/********** STRUCTURES *********/
+
+/********** FUNCTIONS **********/
+
+#endif /* UTILS_H */
